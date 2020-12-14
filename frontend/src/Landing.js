dd33c0de68356d47711f5113f0fa8eb2bccd82d8
xTheQAZx <63619126+xTheQAZx@users.noreply.github.com>
2020-12-14 12:17:02 -0600

    Update Landing.js

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -7,32 +7,33 @@ import { Form, FormControl } from 'react-bootstrap';
 
 function Landing() {
   return (
-    <div style={{ backgroundColor: "#D4CB92" }}>
-      <Jumbotron fluid>
-        <Container>
-          <Image src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" ALIGN="right" />
-          <br />
-          <div style={{ fontSize: "70px" }}>
-            <h1>Strife</h1>
-          </div>
-          <br />
-          <p>
-            Welcome to Strife, a chatting forum for highschool and college students.
+    <div style={{backgroundColor:"#D4CB92"}}>
+  <Jumbotron fluid>
+    <Container>
+      <Image src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" ALIGN="right" />
+      <div style={{ fontSize: "90px" }}>
+        <p>Strife</p>
+      </div>
+      <p>
+        Welcome to Strife, a chatting forum for highschool and college students.
     </p>
-          <br />
-          <p>
-            In order to read or view posts, Strife only asks for your highschool
-            or college name. We ensure that the rest of your identity will be anonymous.
+    <br/>
+      <p>
+        In order to read or view posts, Strife only asks for your highschool
+        or college name. We ensure that the rest of your identity will be anonymous.
     </p>
-          <Form>
-            <Form.Group controlId="formGroupSchool">
-              <Form.Label>Enter School Name</Form.Label>
-              <Form.Control type="school" placeholder="Enter school" />
-            </Form.Group>
-          </Form>
-        </Container>
-      </Jumbotron>
-    </div>
+      <br />
+      <div style={{ width: "45%" }}>
+        <Form>
+          <Form.Group controlId="formGroupSchool">
+            <Form.Label><b>Enter School Name</b></Form.Label>
+            <Form.Control type="school" placeholder="Enter school" />
+          </Form.Group>
+        </Form>
+      </div>
+    </Container>
+  </Jumbotron>
+</div >
   )
 }
 
