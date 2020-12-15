7bedef10a42eb1cfff1ab10f8ea48dcba34c81f5
xTheQAZx <63619126+xTheQAZx@users.noreply.github.com>
2020-12-15 11:46:24 -0600

    Update Landing.js

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -7,33 +7,44 @@ import { Form, FormControl } from 'react-bootstrap';
 
 function Landing() {
   return (
-    <div style={{backgroundColor:"#D4CB92"}}>
-  <Jumbotron fluid>
-    <Container>
-      <Image src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" ALIGN="right" />
-      <div style={{ fontSize: "90px" }}>
-        <p>Strife</p>
+    <div style={{ fontFamily: "Trebuchet MS" }}>
+      <div style={{ backgroundColor: "#D4CB92" }}>
+        <Jumbotron fluid>
+          <Container>
+            <div style={{ textAlign: "center" }}>
+              <div style={{ fontSize: "80px" }}>
+                <p>Strife</p>
+              </div>
+            </div>
+            <Image src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" ALIGN="right" />
+            <br />
+            <div style={{ fontSize: "18px" }}>
+            <p>
+              Welcome to <b>Strife</b>, a chatting forum for highschool and college students.
+          </p>
+          </div>
+            <br />
+            <p>
+              To read or view our posts, Strife asks for your <b>highschool or college name</b>.
+            <br /><br />We ensure that the rest of your identity will be anonymous.
+          </p>
+            <br />
+            <div style={{ width: "45%" }}>
+              <Form>
+                <Form.Group controlId="formGroupSchool">
+                  <Form.Label><b>Enter School Name</b></Form.Label>
+                  <Form.Control type="school" placeholder="Enter school" />
+                </Form.Group>
+              </Form>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+          </Container>
+        </Jumbotron>
       </div>
-      <p>
-        Welcome to Strife, a chatting forum for highschool and college students.
-    </p>
-    <br/>
-      <p>
-        In order to read or view posts, Strife only asks for your highschool
-        or college name. We ensure that the rest of your identity will be anonymous.
-    </p>
-      <br />
-      <div style={{ width: "45%" }}>
-        <Form>
-          <Form.Group controlId="formGroupSchool">
-            <Form.Label><b>Enter School Name</b></Form.Label>
-            <Form.Control type="school" placeholder="Enter school" />
-          </Form.Group>
-        </Form>
-      </div>
-    </Container>
-  </Jumbotron>
-</div >
+    </div>
   )
 }
 
